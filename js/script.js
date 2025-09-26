986aed1758e964f69f0e2e1ef0bb95dab8feeeb4
Samuel Alvarez <abrena2@gmail.com>
2025-09-26 15:23:32 +0200

    Add functionality and grid selector

--- js/script.js
+++ js/script.js
@@ -4,13 +4,13 @@ function fillGrid(size) {
     let mainDiv = document.getElementById("main-container");
     for (let i = 0; i < size; i++) {
         let column = document.createElement("div");
-        column.classList.add("column");
+        column.classList.add("column-div");
         column.style.display = "flex";
         column.style.flexDirection = "column";
         column.style.flex = 1;
         for (let j = 1; j <= size; j++) {
             let row = document.createElement("div");
-            row.classList.add("row");
+            row.classList.add("row-div");
             row.style.border = "1px solid black";
             row.style.flex = 1;
             column.appendChild(row);
@@ -19,4 +19,41 @@ function fillGrid(size) {
     }
 }
 
-fillGrid(70);
+function addEventListenerToGrid() {
+    const grids = document.querySelectorAll(".row-div");
+    let mouseDown = false;
+    grids.forEach((grid) => {
+        //let mouseDown=false;
+        grid.addEventListener("mousedown", (e) => {
+            mouseDown = true;
+            e.preventDefault();
+            //e.target.style.backgroundColor = "black";
+        })
+        grid.addEventListener("mouseenter", (e) => {
+            if (mouseDown) {
+                e.target.style.backgroundColor = "black";
+            }
+        })
+        grid.addEventListener("mouseup", (e) => {
+            mouseDown = false;
+        })
+        grid.addEventListener("click", (e) => {
+            e.target.style.backgroundColor = "black";
+        })
+    })
+}
+
+fillGrid(16);
+addEventListenerToGrid();
+
+const sliderDiv = document.getElementById("slider-div");
+const slider = document.getElementById("grid-range");
+const sliderValue = document.createElement("p");
+sliderValue.textContent = slider.value;
+sliderDiv.appendChild(sliderValue);
+
+//slider.oninput = function() { sliderValue.textContent = this.value }
+
+slider.addEventListener("input", (e) => {
+    sliderValue.textContent = e.target.value;
+})
